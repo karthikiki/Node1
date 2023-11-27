0a60fe9682640a252238e64ddbc706c5b132a7d8
karthikiki <karthickcks003@gmail.com>
2023-11-27 18:36:42 +0530

    Home commit

--- index.js
+++ index.js
@@ -9,8 +9,20 @@ import { userRouter } from "./Routers/users.js";
 dotenv.config();
 const PORT  = process.env.PORT
 const app = express();
+const home = `
+    <h1>Welcome to Student Details API</h1>
+    <br/>
+    <p>GET - /students/all  (Gets All Students)</p>
+    <p>GET - /students/:id  (Get requested student detail)</p>
+    <p>POST - /students/add  (Add New Student Data)</p>
+    <p>PUT - /students/edit/:id  (Update a student detail)</p>
+     <p>DELETE - /students/delete/:id  (Delete a requested student Data)</p>
+    `    
+
 //middleware
 app.use(express.json());
+//Home 
+app.get('/', (req, res) => res.send(home))
 //students routers
 app.use("/students",studentsRouter) 
 //users Router
